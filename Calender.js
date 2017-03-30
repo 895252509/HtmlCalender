30283d72cff51ba0624288c1f532e6d63658e01f
Zhouxiang <895252509@qq.com>
2017-03-30 16:42:14 +0800

    333

--- Calender.js
+++ Calender.js
@@ -7,36 +7,34 @@ var ZxCalender = (function (){
     var _divWidth = 800;
     var _divHeight = 1000;
     var _paddingTB = 100;
-    var _paddingRL = 100;
+    var _paddingRL = null;
     var _colSpace = 100;
     var _rowSpace = 100;
     var _title = _year+'年日历';
     var _colNumber= 3;
+    var _rowNumber =Math.ceil( 12 / _colNumber ) ;
     var _mTableCol = _colNumber*2+1;
     var _monthTableWidth = (_divWidth - 2*_paddingRL )/_colNumber;
     var _monthTableHeight = (_divHeight - 2*_paddingTB )/Math.ceil( 12/_colNumber );
+    var _fontSize = 11;
+    var _monthTablePadding = 2;
+    var _mdiv = document.getElementById(_renderID);
     
     function forDiv(id,year){debugger;
         //初始化数据
         _year = year || _year;
         _renderID = id || _renderID;
-        var mdiv = document.getElementById(_renderID);
-        mdiv.innerHTML = '';
-        _divHeight= mdiv.clientHeight;
-        _divWidth= mdiv.clientWidth;
-        _monthTableWidth = (_divWidth - 2*_paddingRL )/_colNumber;
-        if( _divWidth*0.2 < _colSpace*(_colNumber*2+1)){
-            _colSpace =  _divWidth*0.2 / (_colNumber*2+1);
-        }
-                             
+        doLayout();
+        
+        
         var mtable = document.createElement('table');
         mtable.setAttribute("cellpadding","0");
-        mtable.setAttribute("cellspace","0");
-        var mtableStyle =(mtable.getAttribute("style")||"")+"height:"+_divHeight+"px;width:"+_divWidth+"px;"
+        mtable.setAttribute("cellspacing","0");
+        var mtableStyle =(mtable.getAttribute("style")||"")+"height:"+_divHeight+"px;width:"+_divWidth+"px;"+"padding:0px;margin:0px;";
         mtable.setAttribute("style",mtableStyle);
         var mthead = document.createElement('thead');
         var mtbody = document.createElement('tbody');
-        var mtitle = createFONT(_title); 
+        var mtitle = createFONT(_title,null,20); 
         var titleTd = createTD(_mTableCol);
         titleTd.appendChild(mtitle);
         mthead.appendChild(titleTd);
@@ -72,7 +70,7 @@ var ZxCalender = (function (){
         padding_bottom.appendChild(td);
         mtbody.appendChild(padding_bottom);
         mtable.appendChild(mtbody);
-        mdiv.appendChild(mtable);
+        _mdiv.appendChild(mtable);
     }
     
     function createMonth(month){
@@ -80,8 +78,8 @@ var ZxCalender = (function (){
         //承载月份的表格
         var monthTable = document.createElement('table');
         monthTable.setAttribute("cellpadding","0");
-        monthTable.setAttribute("cellspace","0");
-        var mtableStyle =(monthTable.getAttribute("style")||"")+"height:"+_monthTableHeight+"px;width:"+_monthTableWidth+"px;"
+        monthTable.setAttribute("cellspacing","0");
+        var mtableStyle =(monthTable.getAttribute("style")||"")+"height:"+_monthTableHeight+"px;width:"+_monthTableWidth+"px;padding:"+_monthTablePadding+"px;"
         monthTable.setAttribute("style",mtableStyle);
         //创建一个表头用来显示月份
         var monthHead = document.createElement('thead');
@@ -92,7 +90,7 @@ var ZxCalender = (function (){
         var date = new Date(_year,month,1);
         
         //构建月table对象
-        var titlefont = createFONT(_monthName[month]+'月');
+        var titlefont = createFONT(_monthName[month]+'月',null,_fontSize);
         var titletd = createTD(7);
         titletd.appendChild(titlefont);
         titletd.setAttribute("style","width:100%;text-align:center;");
@@ -142,6 +140,58 @@ var ZxCalender = (function (){
         monthTable.appendChild(monthBody);
         return monthTable;
     }
+    /*doLayout 使用set函数设置样式后 重新计算日历显示参数
+    */
+    function doLayout(){
+        _mdiv = document.getElementById(_renderID);
+        _mdiv.innerHTML = '';
+        var mdivStyle = _mdiv.getAttribute("style")+"overflow:hidden;";
+        //_mdiv.setAttribute("style",mdivStyle);
+        _divHeight= _mdiv.clientHeight;
+        _divWidth= _mdiv.clientWidth;
+        _paddingRL =Math.round( _paddingRL || _divWidth * 0.04);
+        var monthTableMinWidth =Math.round( (_fontSize + 2) * 7 + _monthTablePadding);
+        var monthTableMaxWidth =Math.round( (_fontSize * 2) * 7 + _monthTablePadding);
+        if(monthTableMinWidth*_colNumber > _divWidth){
+            _colSpace = 0;
+            _paddingRL = 2;
+            _monthTableWidth = monthTableMinWidth;
+        }
+        else if(monthTableMaxWidth* _colNumber+_paddingRL*2 < _divWidth){
+            if(_colNumber === 1)
+                _colSpace = _paddingRL = Math.round( (_divWidth- monthTableMaxWidth*_colNumber) / (_colNumber+ 1) );
+            else
+                _colSpace =Math.round( ( (_divWidth - monthTableMaxWidth* _colNumber)- _paddingRL*2 ) / (_colNumber - 1) );
+            _monthTableWidth = monthTableMaxWidth;
+        }else {
+            _colSpace= _paddingRL= Math.round( (_divWidth- monthTableMaxWidth*_colNumber) / (_colNumber+ 1) );
+            _monthTableWidth = monthTableMaxWidth;
+        }
+        
+        
+        _paddingTB =Math.round( _paddingTB || _divHeight * 0.04);
+        var monthTableMinHeight =Math.round( (_fontSize + 2) * 7 + _monthTablePadding);
+        var monthTableMaxHeight =Math.round( (_fontSize * 2) * 7 + _monthTablePadding);
+        if(monthTableMinHeight*_colNumber > _divHeight){
+            _colSpace = 0;
+            _paddingTB = 2;
+            _monthTableHeight = monthTableMinHeight;
+        }
+        else if(monthTableMaxHeight* _colNumber+_paddingTB*2 < _divHeight){
+            if(_colNumber === 1)
+                _colSpace = _paddingTB = Math.round( (_divHeight- monthTableMaxHeight*_colNumber) / (_colNumber+ 1) );
+            else
+                _colSpace =Math.round( ( (_divHeight - monthTableMaxHeight* _colNumber)- _paddingTB*2 ) / (_colNumber - 1) );
+            _monthTableHeight = monthTableMaxHeight;
+        }else {
+            _colSpace= _paddingTB= Math.round( (_divHeight- monthTableMaxHeight*_colNumber) / (_colNumber+ 1) );
+            _monthTableHeight = monthTableMaxHeight;
+        }
+        
+        
+        
+    }
+    
     /*
     
     */
@@ -172,9 +222,9 @@ var ZxCalender = (function (){
         //判断日期 day的 格式是否正确
         // 日历的标题  ‘日’ ‘一’ ‘六’ 等都由此创建
         var td = createTD(1);
-        var font = createFONT(day,thefontColor);
+        var font = createFONT(day,thefontColor,_fontSize);
         td.appendChild(font);
-        var tdstyle =(td.getAttribute("style")||"")+"width:14%;background-color:"+ thebgColor+";";
+        var tdstyle =(td.getAttribute("style")||"")+"width:"+(_fontSize+2)+"px;background-color:"+ thebgColor+";";
         td.setAttribute("style",tdstyle);
         return td;
     }
@@ -184,16 +234,21 @@ var ZxCalender = (function (){
         var thecolspan = colspan || 1;
         var td = document.createElement('td');
         td.setAttribute("colspan",thecolspan);
-        td.setAttribute("style","text-align:center;");
+        var tdstyle =(td.getAttribute("style")||"")+"text-align:center;padding:0px;margin:0px;"
+        td.setAttribute("style",tdstyle);
         return td;
     }
     
     //返回一个字符串，以<font>标签包裹的日期
-    function createFONT(day,fontColor){
+    function createFONT(day,fontColor,fonSize){
         
         var thefontColor = fontColor || 'Black';
+        var thefontSize = fonSize || 10 ;
         var font = document.createElement('font');
+        var theStyle = font.getAttribute("style") || "";
+        theStyle+= "font-size:"+thefontSize+"px;";
         font.setAttribute("color",thefontColor);
+        font.setAttribute("style",theStyle);
         font.innerHTML = day;
         return font;
     }
